73263f9ced230b7c31aa5e7f81654e759f49a76d
dt-dev <desmond.tran0@gmail.com>
2020-05-24 09:47:59 +1000

    Added Functions khelp, ping and month(WIP)

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+#Library Import
 import discord
 from discord.ext import commands
+from datetime import datetime
 
+#Set Summoning Command
 client = commands.Bot(command_prefix = 'k-')
 
+#Get Date
+now = datetime.now()
+month = now.strftime("%m")
+
+#Set permitted commands
+commandDictionary = {
+    "k-khelp": "Displays this menu",
+    "k-ping": "Checks bot's latency/message delay",
+    "k-month": "Shows Current Month's Comebacks"
+}
+
+#Create User Friendly Command Dictionary
+dictionaryToString = ''
+for x in commandDictionary:
+    dictionaryToString += (("**"+x+"**").ljust(10) + "→" + commandDictionary[x]+"\n")
+
+#Set Bot Commands and Events
 @client.event
 async def on_ready():
     print('Bot is ready.')
 
 @client.command()
 async def ping(ctx):
-    await ctx.send('Pong!')
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
-client.run('TOKEN PLACEHOLDER')
+@client.command()
+async def khelp(ctx):
+    await ctx.send('Options:\n'+dictionaryToString)
+
+@client.command()
+async def month(ctx):
+    await ctx.send('Comebacks for the month of ' + str(month) + ': ')
+
+client.run('TOKEN PLACE HOLDER')
