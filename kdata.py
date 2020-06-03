ff5f3daade8fbde20f7bc262f7a2f150d8a0f8e5
dt-dev <desmond.tran0@gmail.com>
2020-06-03 16:59:00 +1000

    hard coding month command

--- kdata.py
+++ kdata.py
@@ -4,12 +4,10 @@ import requests
 from csv import writer
 
 ##Set Global Variables
-global paragraphString
 global table
-global response
 
 ##Get Current Time
-# MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 #
 # now = datetime.now()
 #
@@ -35,30 +33,81 @@ october = current
 november = current
 december = current
 
-response = requests.get('https://dbkpop.com/tag/comebacks')
+def getMonth(month):
+    Found = 0
+    global paragraphString
+    global returnedMonth
 
-soup = BeautifulSoup(response.text, 'html.parser')
+    if month == 1 or month == MONTHS[0]:
+        response = requests.get('https://dbkpop.com/2019/12/02/january-2020-k-pop-comebacks-and-debuts')
+        Found = 1
+        returnedMonth = MONTHS[0]
+    elif month == 2 or month == MONTHS[1]:
+        response = requests.get('https://dbkpop.com/2019/12/26/february-2020-k-pop-comebacks-and-debuts')
+        Found = 1
+        returnedMonth = MONTHS[1]
+    elif month == 3 or month == MONTHS[2]:
+        response = requests.get('https://dbkpop.com/2020/01/23/march-2020-k-pop-comebacks-and-debuts')
+        Found = 1
+        returnedMonth = MONTHS[2]
+    elif month == 4 or month == MONTHS[3]:
+        response = requests.get('https://dbkpop.com/2020/02/04/april-2020-k-pop-comebacks-and-debuts')
+        Found = 1
+        returnedMonth = MONTHS[3]
+    elif month == 5 or month == MONTHS[4]:
+        response = requests.get('https://dbkpop.com/2020/04/03/may-2020-k-pop-comebacks-and-debuts')
+        Found = 1
+        returnedMonth = MONTHS[4]
+    elif month == 6 or month == MONTHS[5]:
+        response = requests.get('https://dbkpop.com/2020/04/20/june-2020-k-pop-comebacks-and-debuts')
+        Found = 1
+        returnedMonth = MONTHS[5]
+    elif month == 7 or month == MONTHS[6]:
+        response = requests.get('https://dbkpop.com/2020/05/11/july-2020-k-pop-comebacks-and-debuts')
+        Found = 1
+        returnedMonth = MONTHS[6]
+    elif month == 8 or month == MONTHS[7]:
+        response = current
+        Found = 0
+        returnedMonth = MONTHS[7]
+    elif month == 9 or month == MONTHS[8]:
+        response = current
+        Found = 0
+        returnedMonth = MONTHS[8]
+    elif month == 10 or month == MONTHS[9]:
+        response = current
+        Found = 0
+        returnedMonth = MONTHS[9]
+    elif month == 11 or month == MONTHS[10]:
+        response = current
+        Found = 0
+        returnedMonth = MONTHS[10]
+    elif month == 12 or month == MONTHS[11]:
+        response = current
+        Found = 0
+        returnedMonth = MONTHS[11]
 
-posts = soup.find_all(class_='site-main')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-for post in posts:
-    title = post.find(class_='entry-title').get_text()
-    body = post.find(class_='entry-content clear')
-    table = str(post.find(id='table_1'))
-    # print(title)
-    # print(body)
-    # print(table)
-paragraphs = body.find_all('p')
-#Convert List of Elements to List of Text Only
-n=0
-paragraphList = []
-paragraphString = ''
-for n in range(len(paragraphs)):
-    paragraphList.append(paragraphs[n].get_text() + '\n\n')
-    n += 1
-for n in range(len(paragraphList)):
-    paragraphString += paragraphList[n]
-#return paragraphString
+    posts = soup.find_all(class_='site-main')
+
+    for post in posts:
+        title = post.find(class_='entry-title').get_text()
+        body = post.find(class_='entry-content clear')
+        table = str(post.find(id='table_1'))
+        # print(title)
+        # print(body)
+        # print(table)
+    paragraphs = body.find_all('p')
+    #Convert List of Elements to List of Text Only
+    n=0
+    paragraphList = []
+    paragraphString = ''
+    for n in range(len(paragraphs)):
+        paragraphList.append(paragraphs[n].get_text() + '\n\n')
+        n += 1
+    for n in range(len(paragraphList)):
+        paragraphString += paragraphList[n]
 
 ##Tables of All Artists
 table = requests.get('https://dbkpop.com/db/all-k-pop-idols')
--- main.py
+++ main.py
@@ -36,7 +36,7 @@ async def help(ctx):
         -k month <month name or number>     Shows comebacks for month specified
         -k subscribe <artist name>          Subscribes you for notifications on a groups comeback
         """,
-        colour=discord.Color.default()
+        colour=discord.Color.purple()
     )
     await ctx.send(embed=helpEmbed)
 
@@ -48,51 +48,13 @@ try:
 
     @client.command()
     async def month(ctx, *, requestedMonth):
-        if requestedMonth == 1 or requestedMonth == MONTHS[0]:
-            requestedMonth = 1
-            kdata.response = kdata.january
-        elif requestedMonth == 2 or requestedMonth == MONTHS[1]:
-            requestedMonth = 2
-            kdata.response = kdata.february
-        elif requestedMonth == 3 or requestedMonth == MONTHS[2]:
-            requestedMonth = 3
-            kdata.response = kdata.march
-        elif requestedMonth == 4 or requestedMonth == MONTHS[3]:
-            requestedMonth = 4
-            kdata.response = kdata.april
-        elif requestedMonth == 5 or requestedMonth == MONTHS[4]:
-            requestedMonth = 5
-            kdata.response = kdata.may
-        elif requestedMonth == 6 or requestedMonth == MONTHS[5]:
-            requestedMonth = 6
-            kdata.response = kdata.june
-        elif requestedMonth == 7 or requestedMonth == MONTHS[6]:
-            requestedMonth = 7
-            kdata.response = kdata.july
-        elif requestedMonth == 8 or requestedMonth == MONTHS[7]:
-            requestedMonth = 8
-            kdata.response = kdata.august
-            await ctx.send("No August Comebacks Have Been Posted, Showing This Months Comebacks")
-        elif requestedMonth == 9 or requestedMonth == MONTHS[8]:
-            requestedMonth = 9
-            kdata.response = kdata.september
-            await ctx.send("No September Comebacks Have Been Posted, Showing This Months Comebacks")
-        elif requestedMonth == 10 or requestedMonth == MONTHS[9]:
-            requestedMonth = 10
-            kdata.response = kdata.october
-            await ctx.send("No October Comebacks Have Been Posted, Showing This Months Comebacks")
-        elif requestedMonth == 11 or requestedMonth == MONTHS[10]:
-            requestedMonth = 11
-            kdata.response = kdata.november
-            await ctx.send("No November Comebacks Have Been Posted, Showing This Months Comebacks")
-        elif requestedMonth == 12 or requestedMonth == MONTHS[11]:
-            requestedMonth = 12
-            kdata.response = kdata.december
-            await ctx.send("No December Comebacks Have Been Posted, Showing This Months Comebacks")
+        Found = 0
+
+        kdata.getMonth(requestedMonth)
         paragraphEmbed = discord.Embed(
-            title=str('Comebacks for the month of ' + MONTHS[int(requestedMonth) - 1] + ': '),
+            title=str('Comebacks for the month of ' + kdata.returnedMonth + ': '),
             description=kdata.paragraphString,
-            colour=discord.Color.default()
+            colour=discord.Color.purple()
         )
 
         # paragraphEmbed.set_footer(text='This is a footer.')
