d4445790c8481df192fc1a42a9fe86ca5d981bff
dt-dev <desmond.tran0@gmail.com>
2020-10-01 12:56:38 +1000

    changed embdedded to darkred

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def current(ctx):
 @client.command()
 async def next(ctx):
     paragraphEmbed = discord.Embed(
-        title = str('Comebacks for the month of November: '),
+        title = str('Comebacks for the month of ' + str(MONTHS[currentMonth]) +': '),
         description = kdata.nextParagraphString,
         colour = discord.Color.dark_red()
     )
